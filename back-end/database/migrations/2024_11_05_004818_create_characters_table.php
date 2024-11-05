c47f0629a36d795dd1ec36dccaf06b0a0faf6fde
Sergio Tavuencas <sergiovicente_tavuenc@outlook.com>
2024-11-05 11:05:39 -0300

    User and character api with authentication

---
+++ back-end/database/migrations/2024_11_05_004818_create_characters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('api_id');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('species');
+            $table->string('gender');
+            $table->json('location');
+            $table->string('image');
+            $table->string('url');
+            $table->timestampsTz(precision: 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characters');
+    }
+};
